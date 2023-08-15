f9dc91a19f5dc8516ca4102d0406df944ee4fffc
Azan <azanbutt2311@gmail.com>
2023-08-15 19:07:28 +0500

    Final

--- test/game4Test.js
+++ test/game4Test.js
@@ -5,15 +5,22 @@ describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
+    const signer = ethers.provider.getSigner(0);
+    const signer1 = ethers.provider.getSigner(1);
 
-    return { game };
+    return { game ,signer , signer1};
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game ,signer ,signer1} = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
+    const address = signer.getAddress();
+    const address1 = signer1.getAddress();
+    
 
-    await game.win();
+     await game.connect(signer).write(address1);
+
+    await game.connect(signer1).win(address);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
